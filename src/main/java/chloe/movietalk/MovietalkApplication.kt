6aa5a28098b936924aeaf09a83ac41e6daab995c
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 16:48:50 +0900

    refactor: clean up Kotlin code after auto-conversion

--- src/main/java/chloe/movietalk/MovietalkApplication.kt
+++ src/main/java/chloe/movietalk/MovietalkApplication.kt
@@ -2,13 +2,13 @@ package chloe.movietalk
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @EnableJpaAuditing
 @SpringBootApplication
-object MovietalkApplication {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        SpringApplication.run(MovietalkApplication::class.java, *args)
-    }
+class MovietalkApplication
+
+fun main(args: Array<String>) {
+    runApplication<MovietalkApplication>(*args)
 }
