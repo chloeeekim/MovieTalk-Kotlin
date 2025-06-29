e60ec342cee78343f3ec7406f410407e9c646c06
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 23:39:55 +0900

    refactor: clean up Actor related Response DTOs Kotlin code after auto-conversion

--- src/main/java/chloe/movietalk/dto/response/actor/ActorInfo.kt
+++ src/main/java/chloe/movietalk/dto/response/actor/ActorInfo.kt
@@ -1,35 +1,32 @@
 package chloe.movietalk.dto.response.actor
 
 import chloe.movietalk.domain.Actor
-import chloe.movietalk.domain.Person.country
-import chloe.movietalk.domain.Person.gender
-import chloe.movietalk.domain.Person.id
-import chloe.movietalk.domain.Person.name
 import chloe.movietalk.domain.enums.Gender
 import io.swagger.v3.oas.annotations.media.Schema
-import lombok.Builder
-import lombok.Getter
-import lombok.NoArgsConstructor
 import java.util.*
 
-@Getter
-@NoArgsConstructor
-class ActorInfo @Builder constructor(
-    @field:Schema(description = "배우 ID") private var id: UUID?, @field:Schema(
-        description = "배우 이름"
-    ) private var name: String?, @field:Schema(description = "성별") private var gender: Gender?, @field:Schema(
-        description = "국적"
-    ) private var country: String?
+data class ActorInfo(
+    @field:Schema(description = "배우 ID")
+    val id: UUID,
+
+    @field:Schema(description = "배우 이름")
+    val name: String,
+
+    @field:Schema(description = "성별")
+    val gender: Gender,
+
+    @field:Schema(description = "국적")
+    val country: String
 ) {
     companion object {
         @JvmStatic
-        fun fromEntity(actor: Actor): ActorInfo? {
-            return ActorInfo.builder()
-                .id(actor.id)
-                .name(actor.name)
-                .gender(actor.gender)
-                .country(actor.country)
-                .build()
+        fun fromEntity(actor: Actor): ActorInfo {
+            return ActorInfo(
+                id = actor.id!!,
+                name = actor.name,
+                gender = actor.gender,
+                country = actor.country
+            )
         }
     }
 }
