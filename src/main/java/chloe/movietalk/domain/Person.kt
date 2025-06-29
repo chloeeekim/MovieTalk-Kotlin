516cabb46217ff11b32d4da9bb0e722775190fc1
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 21:16:30 +0900

    refactor: clean up Kotlin code after auto-conversion

--- src/main/java/chloe/movietalk/domain/Person.kt
+++ src/main/java/chloe/movietalk/domain/Person.kt
@@ -2,25 +2,25 @@ package chloe.movietalk.domain
 
 import chloe.movietalk.domain.enums.Gender
 import jakarta.persistence.*
-import lombok.AccessLevel
-import lombok.Getter
-import lombok.NoArgsConstructor
 import java.util.*
 
 @MappedSuperclass
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-open class Person(
-    @field:Column(nullable = false) private var name: String?, @field:Enumerated(
-        EnumType.STRING
-    ) private var gender: Gender?, @field:Column(length = 50) private var country: String?
+class Person(
+    @Column(nullable = false)
+    var name: String = "",
+
+    @Enumerated(EnumType.STRING)
+    var gender: Gender = Gender.OTHER,
+
+    @Column(length = 50)
+    var country: String = ""
 ) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(nullable = false, unique = true, updatable = false, columnDefinition = "BINARY(16)")
-    private var id: UUID? = null
+    val id: UUID? = null
 
-    fun updatePerson(name: String?, gender: Gender?, country: String?) {
+    fun updatePerson(name: String, gender: Gender, country: String) {
         this.name = name
         this.gender = gender
         this.country = country
