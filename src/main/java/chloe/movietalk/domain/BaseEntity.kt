fdebfed33de7ea8ea8eea82810fb564d2973acf9
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 16:55:38 +0900

    chore: auto-convert BaseEntity from Java to Kotlin

--- src/main/java/chloe/movietalk/domain/BaseEntity.kt
+++ src/main/java/chloe/movietalk/domain/BaseEntity.kt
@@ -1,23 +1,21 @@
-package chloe.movietalk.domain;
+package chloe.movietalk.domain
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
-
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
     @CreatedDate
     @Column(updatable = false, name = "created_at")
-    private LocalDateTime createdAt;
+    private var createdAt: LocalDateTime? = null
 
     @LastModifiedDate
     @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+    private var updatedAt: LocalDateTime? = null
 }
