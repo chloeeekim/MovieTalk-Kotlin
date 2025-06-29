691ca85b68c2aac0faba6c5b2c0ca920f81c77cd
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 22:39:43 +0900

    chore: auto-convert UserRole enum from Java to Kotlin

--- src/main/java/chloe/movietalk/domain/enums/UserRole.kt
+++ src/main/java/chloe/movietalk/domain/enums/UserRole.kt
@@ -1,19 +1,21 @@
-package chloe.movietalk.domain.enums;
+package chloe.movietalk.domain.enums
 
-import chloe.movietalk.exception.global.InvalidRoleEnumValueException;
+import chloe.movietalk.exception.CustomException
+import chloe.movietalk.exception.global.InvalidRoleEnumValueException
+import java.util.*
+import java.util.function.Supplier
 
-import java.util.Arrays;
-
-public enum UserRole {
+enum class UserRole {
     ADMIN,
     USER;
 
-    public static UserRole from(String role) {
-        if (role == null)
-            return null;
-        return Arrays.stream(values())
-                .filter(g -> g.name().equals(role))
+    companion object {
+        fun from(role: String?): UserRole? {
+            if (role == null) return null
+            return Arrays.stream<UserRole?>(entries.toTypedArray())
+                .filter { g: UserRole? -> g!!.name == role }
                 .findFirst()
-                .orElseThrow(() -> InvalidRoleEnumValueException.EXCEPTION);
+                .orElseThrow<CustomException?>(Supplier { InvalidRoleEnumValueException.EXCEPTION })
+        }
     }
 }
