f9dbbdc170186142d72e1ca6315dce93910fb4c7
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 23:40:39 +0900

    chore: auto-convert Director related Response DTOs from JAVA to Kotlin

--- src/main/java/chloe/movietalk/dto/response/director/DirectorDetailResponse.kt
+++ src/main/java/chloe/movietalk/dto/response/director/DirectorDetailResponse.kt
@@ -1,51 +1,45 @@
-package chloe.movietalk.dto.response.director;
+package chloe.movietalk.dto.response.director
 
-import chloe.movietalk.domain.Director;
-import chloe.movietalk.domain.enums.Gender;
-import chloe.movietalk.dto.response.movie.MovieInfo;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.UUID;
+import chloe.movietalk.domain.Director
+import chloe.movietalk.domain.Director.filmography
+import chloe.movietalk.domain.Movie
+import chloe.movietalk.domain.Person.country
+import chloe.movietalk.domain.Person.gender
+import chloe.movietalk.domain.Person.id
+import chloe.movietalk.domain.Person.name
+import chloe.movietalk.domain.enums.Gender
+import chloe.movietalk.dto.response.movie.MovieInfo
+import io.swagger.v3.oas.annotations.media.Schema
+import lombok.Builder
+import lombok.Getter
+import lombok.NoArgsConstructor
+import java.util.*
 
 @Getter
 @NoArgsConstructor
-public class DirectorDetailResponse {
-
-    @Schema(description = "감독 ID")
-    private UUID id;
-
-    @Schema(description = "감독 이름")
-    private String name;
-
-    @Schema(description = "성별")
-    private Gender gender;
-
-    @Schema(description = "국적")
-    private String country;
-
-    @Schema(description = "필모그라피")
-    private List<MovieInfo> filmography;
-
-    @Builder
-    public DirectorDetailResponse(UUID id, String name, Gender gender, String country, List<MovieInfo> filmography) {
-        this.id = id;
-        this.name = name;
-        this.gender = gender;
-        this.country = country;
-        this.filmography = filmography;
-    }
-
-    public static DirectorDetailResponse fromEntity(Director director) {
-        return DirectorDetailResponse.builder()
-                .id(director.getId())
-                .name(director.getName())
-                .gender(director.getGender())
-                .country(director.getCountry())
-                .filmography(director.getFilmography().stream().map(MovieInfo::fromEntity).toList())
-                .build();
+class DirectorDetailResponse @Builder constructor(
+    @field:Schema(description = "감독 ID") private var id: UUID?,
+    @field:Schema(
+        description = "감독 이름"
+    ) private var name: String?,
+    @field:Schema(description = "성별") private var gender: Gender?,
+    @field:Schema(
+        description = "국적"
+    ) private var country: String?,
+    @field:Schema(description = "필모그라피") private var filmography: MutableList<MovieInfo?>?
+) {
+    companion object {
+        @JvmStatic
+        fun fromEntity(director: Director): DirectorDetailResponse? {
+            return DirectorDetailResponse.builder()
+                .id(director.id)
+                .name(director.name)
+                .gender(director.gender)
+                .country(director.country)
+                .filmography(
+                    director.filmography.stream().map<MovieInfo?> { movie: Movie? -> MovieInfo.fromEntity(movie) }
+                        .toList())
+                .build()
+        }
     }
 }
--- src/main/java/chloe/movietalk/dto/response/director/DirectorInfoResponse.kt
+++ src/main/java/chloe/movietalk/dto/response/director/DirectorInfoResponse.kt
@@ -1,44 +1,35 @@
-package chloe.movietalk.dto.response.director;
+package chloe.movietalk.dto.response.director
 
-import chloe.movietalk.domain.Director;
-import chloe.movietalk.domain.enums.Gender;
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
+import chloe.movietalk.domain.Director
+import chloe.movietalk.domain.Person.country
+import chloe.movietalk.domain.Person.gender
+import chloe.movietalk.domain.Person.id
+import chloe.movietalk.domain.Person.name
+import chloe.movietalk.domain.enums.Gender
+import io.swagger.v3.oas.annotations.media.Schema
+import lombok.Builder
+import lombok.Getter
+import lombok.NoArgsConstructor
+import java.util.*
 
 @Getter
 @NoArgsConstructor
-public class DirectorInfoResponse {
-
-    @Schema(description = "감독 ID")
-    private UUID id;
-
-    @Schema(description = "감독 이름")
-    private String name;
-
-    @Schema(description = "성별")
-    private Gender gender;
-
-    @Schema(description = "국적")
-    private String country;
-
-    @Builder
-    public DirectorInfoResponse(UUID id, String name, Gender gender, String country) {
-        this.id = id;
-        this.name = name;
-        this.gender = gender;
-        this.country = country;
-    }
-
-    public static DirectorInfoResponse fromEntity(Director director) {
-        return DirectorInfoResponse.builder()
-                .id(director.getId())
-                .name(director.getName())
-                .gender(director.getGender())
-                .country(director.getCountry())
-                .build();
+class DirectorInfoResponse @Builder constructor(
+    @field:Schema(description = "감독 ID") private var id: UUID?, @field:Schema(
+        description = "감독 이름"
+    ) private var name: String?, @field:Schema(description = "성별") private var gender: Gender?, @field:Schema(
+        description = "국적"
+    ) private var country: String?
+) {
+    companion object {
+        @JvmStatic
+        fun fromEntity(director: Director): DirectorInfoResponse? {
+            return DirectorInfoResponse.builder()
+                .id(director.id)
+                .name(director.name)
+                .gender(director.gender)
+                .country(director.country)
+                .build()
+        }
     }
 }
