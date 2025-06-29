0afa9bd4c76ecffc69fbff340a6d926179153234
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 22:28:11 +0900

    refactor: clean up Kotlin code after auto-conversion

--- src/main/java/chloe/movietalk/domain/enums/Gender.kt
+++ src/main/java/chloe/movietalk/domain/enums/Gender.kt
@@ -1,9 +1,6 @@
 package chloe.movietalk.domain.enums
 
-import chloe.movietalk.exception.CustomException
 import chloe.movietalk.exception.global.InvalidGenderEnumValueException
-import java.util.*
-import java.util.function.Supplier
 
 enum class Gender {
     MALE,
@@ -12,11 +9,10 @@ enum class Gender {
 
     companion object {
         fun from(genderStr: String?): Gender? {
-            if (genderStr == null) return null
-            return Arrays.stream<Gender?>(entries.toTypedArray())
-                .filter { g: Gender? -> g!!.name == genderStr }
-                .findFirst()
-                .orElseThrow<CustomException?>(Supplier { InvalidGenderEnumValueException.EXCEPTION })
+            return genderStr?.let {
+                entries.find { it.name == genderStr }
+                    ?: throw InvalidGenderEnumValueException.EXCEPTION
+            }
         }
     }
 }
