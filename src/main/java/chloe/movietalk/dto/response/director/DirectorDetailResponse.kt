3a36e34650cb6172844dee5bff8470d9647b9735
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 23:51:24 +0900

    refactor: clean up Director related Response DTOs Kotlin code after auto-conversion

--- src/main/java/chloe/movietalk/dto/response/director/DirectorDetailResponse.kt
+++ src/main/java/chloe/movietalk/dto/response/director/DirectorDetailResponse.kt
@@ -1,45 +1,37 @@
 package chloe.movietalk.dto.response.director
 
 import chloe.movietalk.domain.Director
-import chloe.movietalk.domain.Director.filmography
-import chloe.movietalk.domain.Movie
-import chloe.movietalk.domain.Person.country
-import chloe.movietalk.domain.Person.gender
-import chloe.movietalk.domain.Person.id
-import chloe.movietalk.domain.Person.name
 import chloe.movietalk.domain.enums.Gender
 import chloe.movietalk.dto.response.movie.MovieInfo
 import io.swagger.v3.oas.annotations.media.Schema
-import lombok.Builder
-import lombok.Getter
-import lombok.NoArgsConstructor
 import java.util.*
 
-@Getter
-@NoArgsConstructor
-class DirectorDetailResponse @Builder constructor(
-    @field:Schema(description = "감독 ID") private var id: UUID?,
-    @field:Schema(
-        description = "감독 이름"
-    ) private var name: String?,
-    @field:Schema(description = "성별") private var gender: Gender?,
-    @field:Schema(
-        description = "국적"
-    ) private var country: String?,
-    @field:Schema(description = "필모그라피") private var filmography: MutableList<MovieInfo?>?
+data class DirectorDetailResponse(
+    @field:Schema(description = "감독 ID")
+    val id: UUID,
+
+    @field:Schema(description = "감독 이름")
+    val name: String,
+
+    @field:Schema(description = "성별")
+    val gender: Gender,
+
+    @field:Schema(description = "국적")
+    val country: String,
+
+    @field:Schema(description = "필모그라피")
+    val filmography: List<MovieInfo>
 ) {
     companion object {
         @JvmStatic
-        fun fromEntity(director: Director): DirectorDetailResponse? {
-            return DirectorDetailResponse.builder()
-                .id(director.id)
-                .name(director.name)
-                .gender(director.gender)
-                .country(director.country)
-                .filmography(
-                    director.filmography.stream().map<MovieInfo?> { movie: Movie? -> MovieInfo.fromEntity(movie) }
-                        .toList())
-                .build()
+        fun fromEntity(director: Director): DirectorDetailResponse {
+            return DirectorDetailResponse(
+                id = director.id!!,
+                name = director.name,
+                gender = director.gender,
+                country = director.country,
+                filmography = director.filmography.map { MovieInfo.fromEntity(it) }
+            )
         }
     }
 }
--- src/main/java/chloe/movietalk/dto/response/director/DirectorInfoResponse.kt
+++ src/main/java/chloe/movietalk/dto/response/director/DirectorInfoResponse.kt
@@ -1,35 +1,32 @@
 package chloe.movietalk.dto.response.director
 
 import chloe.movietalk.domain.Director
-import chloe.movietalk.domain.Person.country
-import chloe.movietalk.domain.Person.gender
-import chloe.movietalk.domain.Person.id
-import chloe.movietalk.domain.Person.name
 import chloe.movietalk.domain.enums.Gender
 import io.swagger.v3.oas.annotations.media.Schema
-import lombok.Builder
-import lombok.Getter
-import lombok.NoArgsConstructor
 import java.util.*
 
-@Getter
-@NoArgsConstructor
-class DirectorInfoResponse @Builder constructor(
-    @field:Schema(description = "감독 ID") private var id: UUID?, @field:Schema(
-        description = "감독 이름"
-    ) private var name: String?, @field:Schema(description = "성별") private var gender: Gender?, @field:Schema(
-        description = "국적"
-    ) private var country: String?
+data class DirectorInfoResponse(
+    @field:Schema(description = "감독 ID")
+    val id: UUID,
+
+    @field:Schema(description = "감독 이름")
+    val name: String,
+
+    @field:Schema(description = "성별")
+    val gender: Gender,
+
+    @field:Schema(description = "국적")
+    val country: String
 ) {
     companion object {
         @JvmStatic
-        fun fromEntity(director: Director): DirectorInfoResponse? {
-            return DirectorInfoResponse.builder()
-                .id(director.id)
-                .name(director.name)
-                .gender(director.gender)
-                .country(director.country)
-                .build()
+        fun fromEntity(director: Director): DirectorInfoResponse {
+            return DirectorInfoResponse(
+                id = director.id!!,
+                name = director.name,
+                gender = director.gender,
+                country = director.country
+            )
         }
     }
 }
