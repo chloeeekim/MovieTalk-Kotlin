76afa0449499edd60e520433d1276e9b9a473aa0
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-29 18:00:34 +0900

    chore: auto-convert Person from Java to Kotlin

--- src/main/java/chloe/movietalk/domain/Person.kt
+++ src/main/java/chloe/movietalk/domain/Person.kt
@@ -1,41 +1,28 @@
-package chloe.movietalk.domain;
+package chloe.movietalk.domain
 
-import chloe.movietalk.domain.enums.Gender;
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.util.UUID;
+import chloe.movietalk.domain.enums.Gender
+import jakarta.persistence.*
+import lombok.AccessLevel
+import lombok.Getter
+import lombok.NoArgsConstructor
+import java.util.*
 
 @MappedSuperclass
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Person extends BaseEntity {
-
+open class Person(
+    @field:Column(nullable = false) private var name: String?, @field:Enumerated(
+        EnumType.STRING
+    ) private var gender: Gender?, @field:Column(length = 50) private var country: String?
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(nullable = false, unique = true, updatable = false, columnDefinition = "BINARY(16)")
-    private UUID id;
+    private var id: UUID? = null
 
-    @Column(nullable = false)
-    private String name;
-
-    @Enumerated(EnumType.STRING)
-    private Gender gender;
-
-    @Column(length = 50)
-    private String country;
-
-    public Person(String name, Gender gender, String country) {
-        this.name = name;
-        this.gender = gender;
-        this.country = country;
-    }
-
-    public void updatePerson(String name, Gender gender, String country) {
-        this.name = name;
-        this.gender = gender;
-        this.country = country;
+    fun updatePerson(name: String?, gender: Gender?, country: String?) {
+        this.name = name
+        this.gender = gender
+        this.country = country
     }
 }
